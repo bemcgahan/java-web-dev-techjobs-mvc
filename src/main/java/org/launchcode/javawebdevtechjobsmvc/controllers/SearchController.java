2c8619f2150f2d80a69b7f8622a937d1eaf6cace
Brenna McGahan <brenna.degan@gmail.com>
2020-06-09 13:45:16 -0500

    finished search feature

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,18 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        if (searchType.toLowerCase().equals("all") && columnChoices.containsValue(searchTerm)){
+            jobs = JobData.findAll();
 
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
